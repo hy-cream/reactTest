b7c5fa5f8a1ab722458cc5ad9c675426652548f4
lijia <810932574@qq.com>
2017-07-16 22:46:25 +0800

    '增加编辑、添加、删除成员'

--- user-dashboard/src/components/Users/Users.js
+++ user-dashboard/src/components/Users/Users.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import styles from './Users.css';
 import {connect} from 'dva';
-import {Table, Pagination, Popconfirm} from 'antd';
+import {Table, Pagination, Popconfirm, Button} from 'antd';
 import {PAGE_SIZE} from '../../constants';
+import {routerRedux} from 'dva/router';
+import UserModal from './UserModal';
 
-function Users({list: dataSource, total, page:current, loading}) {
+function Users({dispatch, list: dataSource, total, page:current, loading}) {
+  function deleteHandler(id){
+    dispatch({
+      type: 'users/remove',
+      payload: id
+    });
+    console.warn(`todo: ${id}`)
+  }
+  function pageChangeHandler(page){
+    dispatch(routerRedux.push({
+      pathname: '/users',
+      query: {page}
+    }));
+    console.log('page:'+page)
+  }
+  function editHandler(id, values){
+    dispatch({
+      type: 'users/patch',
+      payload:{id, values}
+    })
+  }
+  function createHandler(values) {
+    dispatch({
+      type: 'users/create',
+      payload: values,
+    });
+  }
   const columns = [
     {
       title: 'Name',
@@ -25,26 +53,27 @@ function Users({list: dataSource, total, page:current, loading}) {
     {
        title: 'Operation',
        key: 'operation',
-       render: (text, { id }) => (
+       render: (text, record) => (
         //  operation--?
          <span className={styles.operation}>
-           <a href="">Edit</a>
-           <Popconfirm title="comfirm to delete?" onConfirm={deleteHandler.bind(null,id)}>
+           <UserModal record={record} onOk={editHandler.bind(null,record.id)}>
+            <a>Edit</a>
+           </UserModal>
+           <Popconfirm title="comfirm to delete?" onConfirm={deleteHandler.bind(null, record.id)}>
             <a href="">Delete</a>
            </Popconfirm>
          </span>
       ),
     },
   ]
-  function deleteHandler(id){
-    console.warn(`todo: ${id}`)
-  }
-  function onChange(pageNumber){
-    console.log('page:'+pageNumber)
-  }
   return (
     <div className={styles.normal}>
       <div>
+        <div className={styles.create}>
+          <UserModal record={{}} onOk={createHandler}>
+            <Button type="primary">Create User</Button>
+          </UserModal>
+        </div>
         <Table 
           columns={columns}
           dataSource={dataSource}
@@ -57,14 +86,13 @@ function Users({list: dataSource, total, page:current, loading}) {
           total={total}
           current={current}
           pageSize={PAGE_SIZE}
-          onChange={onChange}
+          onChange={pageChangeHandler}
         />
       </div>
     </div>
   );
 }
 function mapStateToProps(state){
-  console.log(state)
   const {list, total, page} = state.users;
   console.log(state.users.list)
   return{
--- user-dashboard/src/index.js
+++ user-dashboard/src/index.js
@@ -1,14 +1,17 @@
 import dva from 'dva';
 import './index.css';
+import createLoading from 'dva-loading'
 
 // 1. Initialize
 const app = dva();
 
 // 2. Plugins
 // app.use({});
+app.use(createLoading());
 
 // 3. Model
 // app.model(require('./models/example'));
+// app.model(require('./models/users'));
 
 // 4. Router
 app.router(require('./router'));
